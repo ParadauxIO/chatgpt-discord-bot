26be0ef1c534046f117cf6d256946fc822c3dc16
Rían Errity <rian@paradaux.io>
2023-03-05 20:58:19 +0100

    WIP Config loader (untested)

--- src/main/java/io/paradaux/util/ConfigHandler.java
+++ src/main/java/io/paradaux/util/ConfigHandler.java
@@ -1,59 +1,155 @@
 package io.paradaux.util;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.SerializedName;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.spongepowered.configurate.CommentedConfigurationNode;
+import org.spongepowered.configurate.ConfigurateException;
+import org.spongepowered.configurate.hocon.HoconConfigurationLoader;
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 
-import java.io.*;
+import java.nio.file.Path;
 import java.util.HashMap;
 
 public class ConfigHandler {
 
-
-
-    private static final String CONFIG_LOCATION = "/config.json";
+    private static final String CONFIG_LOCATION = "/sylvester.conf";
+    private static final Logger logger = LoggerFactory.getLogger(ConfigHandler.class);
 
     private static Config config;
     public static Config getConfig() {
         return config;
     }
 
+    private static HoconConfigurationLoader loader;
+    private static CommentedConfigurationNode rootNode;
+    public static CommentedConfigurationNode getRootNode() {
+        return rootNode;
+    }
+
     public static Config loadConfig() {
+        IOUtils.deployFiles(logger);
+
+        loader = HoconConfigurationLoader.builder()
+            .path(Path.of(CONFIG_LOCATION)) // or url(), or source/sink
+            .build();
+
         try {
-            IOUtils.deployFiles(LoggerFactory.getLogger(ConfigHandler.class));
+            rootNode = loader.load();
+            config = rootNode.get(Config.class); // Populate object
+        } catch (ConfigurateException e) {
+            logger.error("An error occurred while trying to read the configuration: " + e.getMessage());
+            System.exit(1);
+            return null;
+        }
 
-            GsonBuilder builder = new GsonBuilder();
-            Gson gson = builder.create();
+        logger.info("Successfully loaded configuration.");
+        return config;
+    }
 
-            BufferedReader bufferedReader = new BufferedReader(new FileReader("config.json"));
+    public static Config saveAndReloadConfig() {
+        try {
+            loader.save(rootNode); // Write to the original file
+        } catch (ConfigurateException e) {
+            logger.error("An error occurred while trying to read the configuration: " + e.getMessage());
+            System.exit(1);
+            return null;
+        }
 
-            config = gson.fromJson(bufferedReader, Config.class);
-            return config;
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load config");
+        logger.info("Successfully saved configuration.");
+        return loadConfig();
+    }
+
+    @ConfigSerializable
+    public static class Config {
+        private BotConfig bot;
+        private OpenAIConfig openai;
+
+        public BotConfig bot() {
+            return bot;
+        }
+
+        public OpenAIConfig openai() {
+            return openai;
         }
     }
 
-    public static class Config {
-        @SerializedName("discord_token")
+    @ConfigSerializable
+    public static class BotConfig {
         private String discordToken;
+        private boolean respondOnMention;
+        private boolean respondOnReply;
+        private String[] wakeWords;
+        private double randomResponseChance;
+        private int messageHistoryLength;
+        private HashMap<String, String> channelMapping;
+        private String[] disabledGuilds;
+        private String[] disabledChannels;
 
-        @SerializedName("openai_token")
-        private String openaiToken;
-
-        private HashMap<String, String> guilds;
-
-        public String getDiscordToken() {
+        public String discordToken() {
             return discordToken;
         }
 
-        public String getOpenaiToken() {
-            return openaiToken;
+        public boolean respondOnMention() {
+            return respondOnMention;
         }
 
-        public HashMap<String, String> getGuilds() {
-            return guilds;
+        public boolean respondOnReply() {
+            return respondOnReply;
+        }
+
+        public String[] wakeWords() {
+            return wakeWords;
+        }
+
+        public double randomResponseChance() {
+            return randomResponseChance;
+        }
+
+        public int messageHistoryLength() {
+            return messageHistoryLength;
+        }
+
+        public HashMap<String, String> channelMapping() {
+            return channelMapping;
+        }
+
+        public String[] disabledGuilds() {
+            return disabledGuilds;
+        }
+
+        public String[] disabledChannels() {
+            return disabledChannels;
+        }
+    }
+
+    @ConfigSerializable
+    public static class OpenAIConfig {
+        private String model;
+        private int maxTokens;
+        private int temperature;
+        private int topP;
+        private String prompt;
+
+        public String model() {
+            return model;
+        }
+
+        public int maxTokens() {
+            return maxTokens;
+        }
+
+        public int temperature() {
+            return temperature;
+        }
+
+        public int topP() {
+            return topP;
+        }
+
+        public String prompt() {
+            return prompt;
         }
     }
 
